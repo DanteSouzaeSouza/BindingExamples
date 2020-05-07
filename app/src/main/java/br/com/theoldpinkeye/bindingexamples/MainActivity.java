4a4a0d86c089161973ea69ebb89e3e50ce7abb3a
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-07 15:38:06 -0300

    Correções para Android N (7.0)

--- app/src/main/java/br/com/theoldpinkeye/bindingexamples/MainActivity.java
+++ app/src/main/java/br/com/theoldpinkeye/bindingexamples/MainActivity.java
@@ -34,38 +34,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // liga os Bindings do butterknife a essa Activity
         ButterKnife.bind(this);
+
         // instanciando a nossa lista de UserInfo como um ArrayList
         users = new ArrayList<>();
+
         // adicionando o onClickListener ao objeto Button
-        buttonSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // adicionando o conteúdo preenchido dos campos a uma variável UserInfo
-                UserInfo currentUser = new UserInfo(editTextNome.getText().toString(),
-                        editTextPassword.getText().toString(),
-                        editTextEmail.getText().toString(),
-                        checkBoxAgree.isChecked());
-                // testando jogar no LogCat o conteúdo da variável UserInfo
-                Log.i("Usuário Atual", currentUser.toString());
+        buttonSignUp.setOnClickListener(v -> {
+            // adicionando o conteúdo preenchido dos campos a uma variável UserInfo
+            UserInfo currentUser = new UserInfo(editTextNome.getText().toString(),
+                    editTextPassword.getText().toString(),
+                    editTextEmail.getText().toString(),
+                    checkBoxAgree.isChecked());
+            // testando jogar no LogCat o conteúdo da variável UserInfo
+            Log.i("Usuário Atual", currentUser.toString());
 
-                // adicionando registro ao ArrayList
-                users.add(currentUser);
+            // adicionando registro ao ArrayList
+            users.add(currentUser);
 
-                // limpando a tela
-                editTextNome.setText("");
-                editTextPassword.setText("");
-                editTextPassRepeat.setText("");
-                editTextEmail.setText("");
-                checkBoxAgree.setChecked(false);
-                // colocando o foco de volta no controle do Nome
-                editTextNome.isFocused();
-                // imprimindo os usuários da lista no Logcat
-                for (UserInfo u : users){ // enhanced for
-                    // passará por cada registro dentro do ArrayList e jogará no Logcat
-                    Log.i("Usuário", u.toString());
-                }
+            // limpando a tela
+            editTextNome.setText("");
+            editTextPassword.setText("");
+            editTextPassRepeat.setText("");
+            editTextEmail.setText("");
+            checkBoxAgree.setChecked(false);
+            // colocando o foco de volta no controle do Nome
+            editTextNome.isFocused();
+            // imprimindo os usuários da lista no Logcat
+            for (UserInfo u : users){ // enhanced for
+                // passará por cada registro dentro do ArrayList e jogará no Logcat
+                Log.i("Usuário", u.toString());
             }
         });
     }
